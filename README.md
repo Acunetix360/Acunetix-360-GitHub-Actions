50c1a454d17010c8c7323883a371523fe8d0648c
kiliczsh <muhammeddkilicc@gmail.com>
2021-03-17 15:03:17 +0300

    Implemented Acunetix 360 Scan action.

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-Acunetix-360-GitHub-Actions
+# Acunetix 360 Scan
+
+This action requests a scan on [Acunetix 360](https://acunetix360.com/).
+
+## Inputs
+
+### `website-id`:
+
+**Required** Unique id for your website on Acunetix 360.
+
+### `scan-type`:
+
+**Required** Requested scan type for scan.
+
+### `profile-id`:
+
+**Optional**  Unique profile id for your requested website scan profile on Acunetix 360.
+
+### `user-id`:
+
+**Required** User Id on Acunetix 360 API Credentials. Use GitHub Secrets.
+
+### `api-token`:
+
+**Required** API Token on Acunetix 360 API Credentials. Use GitHub Secrets.
+
+## Outputs
+
+### `scan-message`:
+
+Scan message for requested scan.
+
+## Example Scan Workflow
+
+```yaml
+name: Acunetix 360 Scan Sample Workflow
+
+on:
+  push:
+    branches: [ main ]
+
+jobs:
+  acunetix_scan_job:
+    runs-on: ubuntu-20.04
+    steps:
+      # Clones action repository
+      - name: Checkout action repository
+        id: acunetix-360-scan-action
+        uses: actions/checkout@v2
+        with:
+          repository: Acunetix360/Acunetix-360-GitHub-Actions
+          ref: v0.0.1
+          path: .
+      # Starts actions with given inputs
+      - name: Start Acunetix 360 Scan
+        id: acunetix-360-scan-step
+        uses: ./
+        with:
+          website-id: '******' # FILL HERE
+          scan-type: 'FullWithSelectedProfile'
+          profile-id: '******' # FILL HERE
+          user-id: ${{ secrets.ACUNETIX_USER_ID }}
+          api-token: ${{ secrets.ACUNETIX_API_TOKEN }}
+      # Displays output for action
+      - name: Display Scan Request Message
+        run: echo "${{ steps.acunetix-360-scan-step.outputs.scan-message }}"
+```
---
+++ index.js
@@ -0,0 +1,164 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const axios = require('axios');
+const qs = require('qs');
+
+const baseUrl = 'https://www.acunetix360.com/';
+const createScanRequestEndpoint = 'api/1.0/scans/CreateFromPluginScanRequest';
+const scanStatusPath = 'scans/status/';
+
+const scanTypes = {
+  EMPTY: '',
+  INCREMENTAL: 'Incremental',
+  PRIMARY: 'FullWithPrimaryProfile',
+  SELECTED: 'FullWithSelectedProfile',
+}
+
+const requestType = {
+  GET: 'GET',
+  POST: 'POST',
+}
+
+function isEmpty(str) {
+  return (!str || str === '');
+}
+
+function getScanType(scanTypeEnum) {
+  switch (scanTypeEnum) {
+    case scanTypes.INCREMENTAL:
+      return scanTypes.INCREMENTAL;
+    case scanTypes.PRIMARY:
+      return scanTypes.PRIMARY;
+    case scanTypes.SELECTED:
+      return scanTypes.SELECTED;
+    default:
+      return scanTypes.EMPTY;
+  }
+}
+
+function isScanTypeValid(scanType) {
+  return (
+    scanType === scanTypes.PRIMARY ||
+    scanType === scanTypes.INCREMENTAL ||
+    scanType === scanTypes.SELECTED
+  );
+}
+
+function needsProfile(scanType) {
+  return (
+    scanType === scanTypes.INCREMENTAL ||
+    scanType === scanTypes.SELECTED
+  );
+}
+
+function verifyInputs(websiteIdInput, scanTypeInput, userIdInput, apiTokenInput, scanType, profileIdInput) {
+
+  if (isEmpty(websiteIdInput)) {
+    core.setFailed(`Input website-id is empty.`);
+    return -1;
+  }
+
+  if (isEmpty(userIdInput)) {
+    core.setFailed(`Input user-id is empty.`);
+    return -1;
+  }
+
+  if (isEmpty(apiTokenInput)) {
+    core.setFailed(`Input api-token is empty.`);
+    return -1;
+  }
+
+  if (isEmpty(scanTypeInput)) {
+    core.setFailed(`Input scan-type is empty.`);
+    return -1;
+  }
+
+  if (!isScanTypeValid(scanType)) {
+    core.setFailed(`Input scan-type is not valid: ${scanTypeInput}`);
+    return -1;
+  }
+
+  if (needsProfile(scanType) && isEmpty(profileIdInput)) {
+    core.setFailed(`Input profile-id is empty.`);
+    return -1;
+  }
+
+  return 0;
+}
+
+function prepareRequestData(websiteIdInput, scanType, profileIdInput) {
+  return qs.stringify({
+    'ProfileId': profileIdInput, 'ScanType': scanType, 'WebsiteId': websiteIdInput, "VcsCommitInfoModel": {
+      "CiBuildConfigurationName": `${process.env.GITHUB_REPOSITORY}`, "CiBuildHasChange": `${process.env.GITHUB_SHA}`, "CiBuildId": `${process.env.GITHUB_RUN_NUMBER}`,
+      "CiBuildUrl": `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`, "Committer": `${process.env.GITHUB_ACTOR}`,
+      "IntegrationSystem": "GithubActions", "VcsName": "Git", "VcsVersion": `${process.env.GITHUB_SHA}`
+    }
+  });
+}
+
+function scanRequest() {
+  try {
+    const websiteIdInput = core.getInput('website-id');
+    const scanTypeInput = core.getInput('scan-type');
+    const profileIdInput = core.getInput('profile-id');
+    const userIdInput = core.getInput('user-id');
+    const apiTokenInput = core.getInput('api-token');
+
+    let baseUrlnput = core.getInput('base-url');
+    if (isEmpty(baseUrlnput)) {
+      baseUrlnput = baseUrl;
+    }
+    const scanStatusBaseUrl = baseUrlnput + scanStatusPath;
+    const scanRequestEndpoint = baseUrlnput + createScanRequestEndpoint;
+
+    const scanType = getScanType(scanTypeInput);
+
+    if (verifyInputs(websiteIdInput, scanTypeInput, userIdInput, apiTokenInput, scanType, profileIdInput) === -1) {
+      return -99;
+    }
+
+    var requestData = prepareRequestData(websiteIdInput, scanType, profileIdInput);
+
+    var config = {
+      method: requestType.POST,
+      url: scanRequestEndpoint,
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': `Basic ${Buffer.from(`${userIdInput}:${apiTokenInput}`).toString('base64')}` },
+      data: requestData
+    };
+
+    core.info("Requesting scan...");
+    axios(config).then(function (response) {
+      core.info("Scan request succeeded.");
+      try {
+        var result = JSON.parse(JSON.stringify(response.data));
+      } catch (error) {
+        core.error("Error parsing JSON %s", error.message);
+        throw error;
+      }
+
+      if (result.IsValid) {
+        core.setOutput('scan-message', `Scan details are available at ${String(scanStatusBaseUrl + result.ScanTaskId)}`);
+      } else {
+        throw result;
+      }
+
+    }).catch(function (error) {
+      if (!error.response.data.IsValid) {
+        if (isEmpty(error.response.data.ErrorMessage)) {
+          core.setFailed(`Scan could not be created. Check error: ${error.response.data}`);
+        } else {
+          core.setFailed(`Scan could not be created. Check error message: ${error.response.data.ErrorMessage}`);
+        }
+        return -2;
+      }
+      core.error(`Error: ${error.response.data}`);
+      return -3;
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+    return -4;
+  }
+}
+
+// MAIN
+scanRequest();
